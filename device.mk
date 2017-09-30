87c872b5e886280062199e1a1f1da80a0bf5729d
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 19:34:50 -0400

    X00P: Build netd HAL and netutils wrapper
    Needed by netmgrd.
    
    Change-Id: I93453934a2f3e06296eb4f7a62941b9b6abca703

--- device.mk
+++ device.mk
@@ -158,6 +158,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Net
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.0 \
+    netutils-wrapper-1.0
+
 # Power
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
