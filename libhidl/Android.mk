9db857e17fd92e40f2767ba0d69b5be44cb61524
dianlujitao <dianlujitao@lineageos.org>
2018-01-04 09:48:59 +0000

    X00P: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: Idae6c4fedd687e8fe1ce24f680f5f386862c12bb

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2020-21 The Android Open-Source Project
-# Copyright (C) 2020-21 The LineageOS Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,24 +12,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Get non-open-source specific aspects
-$(call inherit-product, vendor/asus/X00P/X00P-vendor.mk)
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_MODULE := android.hidl.base@1.0
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
 
-# Screen density
-TARGET_SCREEN_HEIGHT := 1440
-TARGET_SCREEN_WIDTH := 720
-
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.manager@1.0 \
-    android.hidl.base@1.0
-
-# Ramdisk
-PRODUCT_PACKAGES += \
-    fstab.qcom
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_MODULE := android.hidl.manager@1.0
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
