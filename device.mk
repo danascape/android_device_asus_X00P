2555cc50f0caf805bad36e23a36c1e4723861a0e
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    X00P: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    
    Signed-off-by: Demon Singur <demonsingur@gmail.com>
    Change-Id: I79a74665b8ec6782f4a583ab0bbee00ff3b24b06

--- device.mk
+++ device.mk
@@ -28,8 +28,10 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.base@1.0
+    android.hidl.manager@1.0_system
 
 # Ramdisk
 PRODUCT_PACKAGES += \
