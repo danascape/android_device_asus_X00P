3cce5910bb9c70f55608d09f4ec80ee6a59fee31
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:07:22 +0200

    X00P: Don't build vndk_package
    * It's built by default if vndk is set to current
    
    Change-Id: I2a00446046bafe265e5ac894656abc72514319d8

--- device.mk
+++ device.mk
@@ -436,10 +436,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK
-PRODUCT_PACKAGES += \
-    vndk_package
-
 # Wifi
 PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
