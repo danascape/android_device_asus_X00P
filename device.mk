bb295536e6677e5b93bacfd08b7b889c01165097
Peter Cai <peter@typeblog.net>
2019-10-11 16:12:06 +0800

    X00P: Build lineage power HAL 1.2
    Change-Id: Idd94e6662429afbba612bc2edf8e2f6e9292fa25

--- device.mk
+++ device.mk
@@ -247,6 +247,9 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service-qti
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
