c8982ecf30b20f0aa8104eee0262fceb835ba552
ShubhamB <shubhamprince111@gmail.com>
2020-02-20 13:41:50 +0000

    X00P: Build libsensorndkbridge
    Change-Id: I53074a08007ef2e6e2545c810f5ca6d1d060ad15

--- device.mk
+++ device.mk
@@ -350,6 +350,9 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    libsensorndkbridge
+
+PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
