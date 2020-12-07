83ab1e5011c64b2a7b709010070cf5c9f12400dc
danascape <saalimquadri1@gmail.com>
2020-12-07 15:27:54 +0530

    X00P: Build thermal HAL
    Change-Id: I871e3b67e11cc61caf5847f8d9339b99a875fe5f

--- device.mk
+++ device.mk
@@ -287,6 +287,13 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+PRODUCT_PACKAGES += \
+    thermal.msm8937
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
