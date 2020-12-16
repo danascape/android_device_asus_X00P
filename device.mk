b4de6a1cce2f79993fd248c6aa11f876f16c110a
Pawin Vongmasa <pawin@google.com>
2020-12-16 05:35:02 +0000

    X00P: Build Codec2 packages
    Change-Id: I291248b27e903b23be44b57c4fc33373d3e6712e

--- device.mk
+++ device.mk
@@ -311,6 +311,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Media - Codec2 modules
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec \
+    libsfplugin_ccodec
+
 # Media - OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
