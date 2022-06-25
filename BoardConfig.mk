6e5eb9d414bc670480ec0e8ba7c21f2b4949ceae
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-26 02:25:18 +0800

    X00P: Add `system_ext` `product` `odm` partitions
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    
    # Conflicts:
    #	rootdir/etc/fstab.qcom

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,8 +149,8 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 80
 
 # Partitions - Super
-SSI_PARTITIONS := system
-TREBLE_PARTITIONS := vendor
+SSI_PARTITIONS := product system system_ext
+TREBLE_PARTITIONS := odm vendor
 ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
 
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
