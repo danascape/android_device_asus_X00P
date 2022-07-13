0d82d8a3951c7f14b86d921f0392ce90a65b9a87
danascape <danascape@gmail.com>
2022-07-13 07:29:03 -0600

    X00P: Trim down Lunch Choices
    Change-Id: Id2a18e3645a1505a056e51d5d487f52b60ba61bc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_X00P.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00P-user \
-    lineage_X00P-userdebug \
-    lineage_X00P-eng
+    lineage_X00P-userdebug
