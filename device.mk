55a3ad009542ae44f1abf5303636ac5dca00c0b0
danascape <danascape@gmail.com>
2022-07-14 10:13:30 -0600

    X00P: Drop atrace HAL
    * Throws an exception.
    
    Change-Id: I5af8056853058ad29be7debb5c5af5dea83d84a9

--- device.mk
+++ device.mk
@@ -81,10 +81,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     libaudioroute \
