504fe27b5efd945512becfd985f612425fc3107d
Saalim Quadri <danascape@gmail.com>
2023-05-30 00:50:22 +0530

    X00P: Drop FDE
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -267,9 +267,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcrypto
 
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
