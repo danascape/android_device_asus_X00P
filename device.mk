390ade4b1f370d958730b943338a4c40309ca7b2
Saalim Quadri <danascape@gmail.com>
2023-05-30 16:00:42 +0530

    X00P: Remove deprecated audio.effect target
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -112,7 +112,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@6.0 \
     android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service
