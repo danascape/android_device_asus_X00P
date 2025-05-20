fcb6919bd070086f7acf5ad3cdd75f55ca66de35
Saalim Quadri <danascape@gmail.com>
2025-05-20 05:53:37 +0000

    X00P: Also show sepolicy in namespace
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -29,7 +29,8 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 
 # Soong Namespace
 PRODUCT_SOONG_NAMESPACES += \
-    device/asus/X00P
+    device/asus/X00P \
+    device/asus/X00P-sepolicy
 
 # Screen density
 TARGET_SCREEN_HEIGHT := 1440
